b97a8a0f1f5ff27a8a4761613e9d5ea3d3f12663
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-04-05 18:23:55 +0300

    ещё чуть чуть веселья с flex перевёл на С++

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # flex test.l && bison test.y && gcc lex.yy.c && cat test.foo | ./a.out
-flex test.l && gcc lex.yy.c && cat test.foo | ./a.out
+
+flex --c++ test.l && g++ lex.yy.cc && cat test.foo | ./a.out
