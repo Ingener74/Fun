44a4616488796ef3d40d6198cf218c74cda2944f
ShnaiderPavel <shnaiderpasha@gmail.com>
2016-04-13 09:42:23 +0300

    добавил ещё одиин пример

--- Deserted-Xylophone/main.cpp
+++ Deserted-Xylophone/main.cpp
@@ -22,6 +22,8 @@ int main(int argc, char *argv[]) {
 
         Module* module = makeModule();
 
+        module->dump();
+
         verifyModule(*module/*, */);
 
         ModulePassManager mpm;
---
+++ Tainted-Bulldozer/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <llvm/>
+
+using namespace std;
+using namespace llvm;
+
+int main(int argc, char *argv[]) {
+    try {
+
+        Initialli
+
+    } catch (const exception &e) {
+        cerr << e.what() << endl;
+    }
+}
