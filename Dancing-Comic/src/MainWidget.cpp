bac10f59816f52207de3f6c369760a1f9dd90acb
Shnaider Pavel <shnaiderpasha@gmail.com>
2016-04-28 22:28:26 +0400

    добавил шрифт и иконку, и сборал гуи фронтэнд для сырой дороги

--- Dancing-Comic/src/MainWidget.cpp
+++ Dancing-Comic/src/MainWidget.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <QtGui/QKeyEvent>
+#include <QtGui/QFontDatabase>
 #include <QtCore/QTimer>
 #include <QtWidgets/QFileDialog>
 #include <QtWidgets/QMessageBox>
@@ -12,7 +13,13 @@ using namespace std;
 MainWidget::MainWidget(QWidget* parent, Qt::WindowFlags f) :
     QWidget(parent, f) {
     setupUi(this);
+	
+	int id = QFontDatabase::addApplicationFont(":/fonts/DroidSansMono.ttf");
+	QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+	QFont monospace(family);
 
+	codeTextEdit->setFont(monospace);
+	consoleTextEdit->setFont(monospace);
 }
 
 MainWidget::~MainWidget() {
