f4d6d330ad6015fdf19ecc93310e9e3df9256b0d
Shnaider Pavel <shnaiderpasha@gmail.com>
2016-10-17 00:06:44 +0300

    бекенд командной строки

--- fun/main.cpp
+++ fun/main.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 using namespace fun;
 using namespace Poco;
-using namespace Poco::Util;
+using namespace Util;
 
 class ConsoleDebugger : public Debugger {
 public:
@@ -26,13 +26,13 @@ public:
     virtual ~ConsoleDebugger() {
     }
 
-    virtual void onOperandsChanged(const std::vector<Terminal*> &operands) override {
+    virtual void onOperandsChanged(const std::vector<Terminal*> &) override {
     }
 
-    virtual void onMemoryChanged(const std::unordered_map<std::string, Terminal*>& memory) override {
+    virtual void onMemoryChanged(const std::unordered_map<std::string, Terminal*>&) override {
     }
 
-    virtual void onCatchBreakpoint(const Breakpoint &breakpoint) override {
+    virtual void onCatchBreakpoint(const Breakpoint &) override {
     }
 };
 
@@ -46,113 +46,144 @@ bool parseAndRunCode(Visitor* visitor, const string& filename, istream& inputStr
     return result;
 }
 
-class FunApplication: public Poco::Util::Application {
+class FunApplication: public Application {
 public:
     FunApplication() {
     }
 
 protected:
-    void initialize(Application& self) {
+    void initialize(Application& self) override {
         Application::initialize(self);
     }
 
-    void uninitialize() {
+	void uninitialize() override {
         Application::uninitialize();
     }
 
-    void reinitialize(Application& self) {
+	void reinitialize(Application& self) override {
         Application::reinitialize(self);
     }
 
-    void defineOptions(OptionSet& options) {
+	void defineOptions(OptionSet& options) override {
         Application::defineOptions(options);
 
         options.addOption(
-                Option("help", "h", "help information")
-                .required(false)
-                .repeatable(false)
-                .callback(OptionCallback<FunApplication>(this, &FunApplication::handleHelp)));
+            Option("help", "h", "help information")
+            .required(false)
+            .repeatable(false)
+            .callback(OptionCallback<FunApplication>(this, &FunApplication::handleHelp)));
 
         options.addOption(
-                Option("debug", "d", "debug mode")
-                .required(false)
-                .repeatable(false)
-                .callback(OptionCallback<FunApplication>(this, &FunApplication::handleDebug)));
+            Option("debug", "d", "debug mode")
+            .required(false)
+            .repeatable(false)
+            .callback(OptionCallback<FunApplication>(this, &FunApplication::handleDebug)));
 
         options.addOption(
-                Option("file", "f", "input script")
-                .required(true)
-                .repeatable(false)
-                .argument("file", true)
-                .callback(OptionCallback<FunApplication>(this, &FunApplication::handleScript)));
+            Option("file", "f", "input script")
+            .required(false)
+            .repeatable(false)
+            .argument("file", true)
+            .callback(OptionCallback<FunApplication>(this, &FunApplication::handleScript)));
+
+		options.addOption(
+			Option("print", "p", "Print program")
+			.callback(OptionCallback<FunApplication>(this, &FunApplication::handlePrint)));
 
         options.addOption(
-            Option("watch", "w", "watch for auto reload")
-            .callback(OptionCallback<FunApplication>(this, &FunApplication::handleWatch))
+			Option("watch", "w", "watch for auto reload")
+			.callback(OptionCallback<FunApplication>(this, &FunApplication::handleWatch))
         );
     }
 
     void handleHelp(const std::string& name, const std::string& value) {
-        cout << __PRETTY_FUNCTION__ << ", " << name << ": " << value << endl;
+		_help = true;
     }
 
     void handleDebug(const std::string& name, const std::string& value) {
-        cout << __PRETTY_FUNCTION__ << ", " << name << ": " << value << endl;
+		_debug = true;
     }
 
     void handleWatch(const std::string& name, const std::string& value) {
-        cout << __PRETTY_FUNCTION__ << ", " << name << ": " << value << endl;
+		_watch = true;
+    }
+
+	void handlePrint(const std::string& name, const std::string& value) {
+		_print = true;
     }
 
     void handleScript(const std::string& name, const std::string& value) {
         scriptFileName = value;
     }
 
-    void displayHelp() {
-//        HelpFormatter helpFormatter(options());
-//        helpFormatter.setCommand(commandName());
-//        helpFormatter.setUsage("OPTIONS");
-//        helpFormatter.setHeader(
-//                "A sample application that demonstrates some of the features of the Poco::Util::Application class.");
-//        helpFormatter.format(std::cout);
-    }
-
-    int main(const ArgVec& args) {
+	int main(const ArgVec& args) override {
 
         try {
-            Printer printer;
-            ConsoleDebugger consoleDebugger(&printer);
-            Interpreter interpret(nullptr);
-            Compiler compiler;
+			if (scriptFileName.empty())
+			{				
+				stringstream sourceStream;
+				while (true) {
+					cout << ">>> ";
 
-            Visitor* visitor = &interpret;
+					string input;
+					getline(cin, input);
 
-            string filename = scriptFileName;
-            if (scriptFileName.empty())
-                throw std::runtime_error("no input file");
-            ifstream file(filename);
+					if (input == "quit") {
+						return 0;
+					}
+					else if (!input.empty()) {
+						sourceStream << input << endl;
+						continue;
+					}
 
-            Thread th;
-            th.startFunc([visitor, &file, &filename] {
-                try {
-                    parseAndRunCode(visitor, filename, file, false);
-                } catch (std::exception &e) {
-                    cerr << e.what() << endl;
-                }
-            });
+					// parseAndRunCode(visitor, {}, sourceStream, _debug);
 
-            if (th.isRunning()) {
-                th.join();
-            }
+					sourceStream.clear();
+				}
+			}
+			else
+			{
+				Printer printer;
+				ConsoleDebugger consoleDebugger(&printer);
+				Interpreter interpret(nullptr);
+				Compiler compiler;
 
-            return Application::EXIT_OK;
+				Visitor* visitor = &interpret;
+
+				string filename = scriptFileName;
+				if (scriptFileName.empty())
+					throw std::runtime_error("no input file");
+				ifstream file(filename);
+
+				Thread th;
+				th.startFunc([visitor, &file, &filename] {
+					try {
+						parseAndRunCode(visitor, filename, file, false);
+					}
+					catch (std::exception &e) {
+						cerr << e.what() << endl;
+					}
+				});
+
+				if (th.isRunning()) {
+					th.join();
+				}
+			}
+
+			return EXIT_OK;
         } catch (exception const& e) {
-            return Application::EXIT_SOFTWARE;
+			cerr << e.what() << endl;
+            return EXIT_SOFTWARE;
         }
     }
 
 private:
+	bool _print = false;
+	bool _watch = false;
+	bool _debug = false;
+	bool _help = false;
     string scriptFileName;
+
 };
 
 POCO_APP_MAIN(FunApplication)
